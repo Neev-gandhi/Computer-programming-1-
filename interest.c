8100dca1a4d29eb7f51fbcaa5c880ba885772bc4
Neev-gandhi <gandhineev21@gmail.com>
2025-11-10 19:59:13 +0530

    Delete interest.c

--- interest.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    float p,r,n,i;
-    printf("enter principal amount,rate and time",p,r,n);
-    scanf("%f%f%f",&p,&r,&n);
-    i = p*r*n/100;
-    printf("the interest is %.2f",i);
-}
