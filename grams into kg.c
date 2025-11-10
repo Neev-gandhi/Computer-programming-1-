e05d77a3c8be1e184958c39c18b8ec13c3e9e6cd
Neev-gandhi <gandhineev21@gmail.com>
2025-11-10 19:58:50 +0530

    Delete grams into kg.c

--- grams into kg.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-    void main()
-    {
-    float a,b;
-    printf("enter weight in grams");
-    scanf("%f",&a);
-    b=a/1000;
-    printf("the weight in kilograms is %f kg ",b);
-    }
