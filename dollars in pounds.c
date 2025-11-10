6d74d23a380a1441221eb09385f800254ce7a862
Neev-gandhi <gandhineev21@gmail.com>
2025-11-10 19:58:14 +0530

    Delete dollars in pounds.c

--- dollars in pounds.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-    void main()
-    {
-    float a,b;
-    printf("enter amount in dollars");
-    scanf("%f",&a);
-    b=a*0.74;
-    printf("%f dollars equals to %f pounds",a,b);
-    }
